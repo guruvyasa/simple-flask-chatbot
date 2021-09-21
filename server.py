b4ec0af7a2006318362554e80b59ab544593f93a
chandan purohit <vyasapurohit@gmail.com>
2021-09-21 13:52:15 +0530

    added chatterbot

--- server.py
+++ server.py
@@ -4,6 +4,29 @@ standard_answers = {
     "what is your name?":"My name is Sathvik Bot!"
 }
 chat_history = []
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import logging
+import joblib
+
+
+'''
+This is an example showing how to train a chat bot using the
+ChatterBot Corpus of conversation dialog.
+'''
+
+# Enable info level logging
+logging.basicConfig(level=logging.INFO)
+
+chatbot = ChatBot('Example Bot')
+
+# Start by training our bot with the ChatterBot corpus data
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train(
+    'chatterbot.corpus.english'
+)
+
 
 @app.route("/")
 def index():
@@ -18,9 +41,10 @@ def home():
         return render_template("chat.html", messages=chat_history)
     else:
         user_response = request.form.get("input")
-        ans = standard_answers.get(user_response.lower(),"Sorry, I could not understand")
+        ans = chatbot.get_response(user_response)
+        # ans = standard_answers.get(user_response.lower(),"Sorry, I could not understand")
         chat_history.append("You: "+user_response)
-        chat_history.append("Me: "+ans)
+        chat_history.append("Me: "+str(ans))
 
         return render_template("chat.html", messages = chat_history)
-app.run(debug=True)
+app.run(debug=True, threaded=False)
